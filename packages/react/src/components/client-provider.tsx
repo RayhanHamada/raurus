2d51617cceb9ce5e2419a1d41457eb80d28cfd2d
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2026-07-03 02:55:00 +0700

    refactor: remove initialData prop and update placeholders initialization in RaurusClientProvider

--- packages/react/src/components/client-provider.tsx
+++ packages/react/src/components/client-provider.tsx
@@ -6,23 +6,20 @@ import { RaurusContext } from "@/context";
 
 export interface RaurusClientProviderProps {
     url: string | URL;
-    initialData?: Data[];
     editMode?: boolean;
 }
 
-const DEFAULT_INITIAL_DATA = [] as Data[];
 const DEFAULT_EDIT_MODE = false;
 
 export const RaurusClientProvider: FC<PropsWithChildren<RaurusClientProviderProps>> = ({
     children,
     editMode: _editMode = DEFAULT_EDIT_MODE,
-    initialData = DEFAULT_INITIAL_DATA,
 }) => {
     const [editMode, setEditMode] = useState(_editMode);
     const [selectedId, setSelectedId] = useState<string | null>(null);
     const [editingId, setEditingId] = useState<string | null>(null);
 
-    const placeholders = useRef(new Map(initialData.map((v) => [v.placeholder_id, v])));
+    const placeholders = useRef(new Map<string, Data>());
 
     const getById = useCallback((id: string) => placeholders.current.get(id), []);
 
