67795c8136fdff2f532298974cb6f80a25d955dc
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2026-07-13 23:16:17 +0700

    refactor: remove example-server and mvp-editable-field examples
    - Deleted example-server directory including package.json, index.ts, and tsconfig.json.
    - Removed mvp-editable-field directory including all related files such as README.md, index.html, package.json, and source files.
    - Updated next-example layout to use `enableEdit` prop instead of `editMode`.
    - Updated package.json dependencies for improved versions and removed unused packages.

--- examples/next-example/src/app/layout.tsx
+++ examples/next-example/src/app/layout.tsx
@@ -26,7 +26,7 @@ const Layout: FC<Props> = ({ children }) => {
     return (
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
             <body className="min-h-full flex flex-col">
-                <RaurusClientProvider url="http://localhost:3000" editMode={true}>
+                <RaurusClientProvider url="http://localhost:3000" enableEdit>
                     {children}
                 </RaurusClientProvider>
             </body>
